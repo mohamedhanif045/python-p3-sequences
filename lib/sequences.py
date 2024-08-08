a88a085644b6966a4081c677e4aca024c1dc461f
mohamedhanif045 <hanifmohamed045@gmail.com>
2024-08-08 17:43:50 +0300

    updated python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+
+    # Generate the Fibonacci sequence up to the given length
+    for i in range(length):
+        if i == 0:
+            fibonacci_list.append(0)
+        elif i == 1:
+            fibonacci_list.append(1)
+        else:
+            fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
+    
+    # Print the result
+    print(fibonacci_list)
